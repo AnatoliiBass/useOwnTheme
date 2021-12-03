c61b0c566e449713940deaf50b1b018949641768
AnatoliiBass <bass_1304@ukr.net>
2021-12-03 10:17:26 +0100

    create tables to use recurtion

--- themes/generatorTable/assets/js/main.js
+++ themes/generatorTable/assets/js/main.js
@@ -1,4 +1,4 @@
-import * as data from 'json/test2.json';
+import * as data from 'json/test.json';
 const container = document.getElementById('container')
 //function Flatten is making simple Array for next operations
 const flatDeep = (arr, d = 1) => {
@@ -6,6 +6,7 @@ const flatDeep = (arr, d = 1) => {
 }
 //function isEmpty return true if json has empty objects or array, 
 //return false if json is not empty and remove empty elements inside array
+// and return new array
 const isEmpty = (obj = {}) => {
    let isEmpty, count = 0, returnObj
    const deleteEmptyItems = (obj, count) => {
@@ -14,7 +15,6 @@ const isEmpty = (obj = {}) => {
    if (Array.isArray(obj)) {
       returnObj = flatDeep(obj, Infinity)
       const copy = [...returnObj]
-      console.log(copy);
       if (Object.keys(copy).length === 0) {
          isEmpty = true
       } else {
@@ -24,7 +24,6 @@ const isEmpty = (obj = {}) => {
                isEmpty = false
                count += 1
             } else {
-               console.log(returnObj, count);
                deleteEmptyItems(returnObj, count)
             }
          })
@@ -38,14 +37,59 @@ const isEmpty = (obj = {}) => {
    }
    return {
       empty: isEmpty,
-      newData: returnObj
+      newData: returnObj ? returnObj : [obj]
    }
 }
+//function creates all tables from json to use a recurtion
+const createTable = (infoArray) => {
+   const keys = Object.keys(infoArray[0])
+   const table = document.createElement('table')
+   table.setAttribute('border', '2')
+   const caption = document.createElement('caption')
+   caption.textContent = `Table #${numberTable}`
+   table.append(caption)
+   numberTable++
+   const tbody = document.createElement('tbody')
+   const trh = document.createElement('tr')
+   const trd = document.createElement('tr')
+   keys.forEach(item => {
+      const th = document.createElement('th')
+      th.textContent = item
+      trh.append(th)
+   })
+   tbody.append(trh)
+   infoArray.forEach(element => {
+      const values = Object.values(element)
+      values.forEach(item => {
+         if (item instanceof Object) {
+            const { empty, newData } = isEmpty(item)
+            console.log(item, newData);
+            if (!empty) {
+               if (newData[0] instanceof Object) {
+                  createTable(newData)
+               }
+            }
+         }
+         const td = document.createElement('td')
+         td.textContent = item
+         trd.append(td)
+      })
+      tbody.append(trd)
+   })
 
-console.log(data.default)
+   table.append(tbody)
+   container.appendChild(table)
+}
+
 const { empty, newData } = isEmpty(data.default)
-console.log(empty, newData)
+let numberTable = 1
+console.log(empty, newData);
+if (empty) {
+   container.innerHTML = `<h1>Your JSON is empty</h1>`
+} else {
+   createTable(newData)
+}
 
-// console.log(isEmpty(data.default))
+console.log([] instanceof Object)
 
 // console.log(data.default)
